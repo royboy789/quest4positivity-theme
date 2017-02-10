6834d0af5dfcdda077c569b8324edda1a077beaa
Roy Sivan <roy.sivan@disney.com>
2017-02-10 14:38:14 -0800

    init commit

---
+++ functions.php
@@ -0,0 +1,33 @@
+<?php
+
+define( 'MY_THEME_BASE_PATH', get_template_directory() );
+define( 'MY_THEME_BASE_URI', get_template_directory_uri() );
+define( 'MY_THEME_ASSETS_URI', MY_THEME_BASE_URI.'/assets' );
+define( 'MY_THEME_BUILD_URI', MY_THEME_BASE_URI.'/build' );
+define( 'MY_THEME_VERSION', '1.0' );
+
+require_once get_template_directory() . '/includes/theme-enqueue.php';
+
+class my_theme {
+
+	protected $theme_enqueue;
+
+	function __construct() {
+		$this->theme_enqueue = new my_theme_enqueue();
+
+	}
+
+	function theme_enqueue() {
+		$this->theme_enqueue->theme_scripts();
+	}
+	function theme_setup() {
+		add_theme_support( 'title-tag' );
+	}
+}
+
+$my_theme = new my_theme();
+
+add_action( 'wp_enqueue_scripts', array( $my_theme, 'theme_enqueue' ) );
+add_action( 'after_setup_theme', array( $my_theme, 'theme_setup' ) );
+
+?>
---
+++ header.php
@@ -0,0 +1,44 @@
+<!doctype html>
+<html>
+	<head>
+	<meta charset="<?php bloginfo( 'charset' ); ?>" />
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<?php wp_head(); ?>
+	<!--[if lt IE 9]>
+		<script src="//html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+	<![endif]-->
+	</head>
+	<body <?php body_class(); ?>>
+	<header class="container-fluid">
+		<div class="container">
+			<div class="row">
+				<div class="col-sm-2">
+					<div class="logo">
+						Q4P
+						<span>Quest 4 Positivity</span>
+					</div>
+				</div>
+				<div class="col-sm-10">
+					<div class="row">
+						<div class="col-sm-5 text-center tagline">
+							Words of Wisdom by Justin Scarred
+							<em>and The Sometimes Mob</em>
+						</div>
+						<div class="col-sm-7 text-right">
+							<nav>
+								<ul>
+									<li><a href="https://www.youtube.com/user/justinscarred">Justin Scarred Videos</a></li>
+									<li><a href="https://www.facebook.com/groups/thesometimesmob">The Sometimes Mob</a></li>
+									<li><a href="https://www.patreon.com/justinscarred">Patreon</a></li>
+									<li><a href="https://shop.spreadshirt.com/livefastdiepoor/">Shop</a></li>
+								</ul>
+							</nav>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</header>
+	<div class="container-fluid">
+		<div class="container">
