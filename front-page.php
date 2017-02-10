9e24ce8d854fd9dfd3bda40a9b2fb2004dceadda
Roy Sivan <roy.sivan@disney.com>
2017-02-10 15:32:07 -0800

    API connection

--- front-page.php
+++ front-page.php
@@ -15,15 +15,40 @@
 		<p>
 			Share rules you have to get through your quest. Each one of us is different, but can learn from each other on our own quests.
 		</p>
-		<form class="form">
+		<hr />
+		<div id="schleep_well" style="display: none" class="text-center">
+			<h4>You've Done Your Duty!</h4>
+			<img src="<?php echo get_template_directory_uri(). '/schleep_well.jpg'; ?>" class="img-responsive" alt="Justin Scarred Quest For Positivity" />
+		</div>
+		<form class="form" id="share_quest_form">
 			<div class="form-group">
 				<label for="quest_text">What rule do you have for <strong>your</strong> Quest 4 Positivity?</label>
 				<textarea id="quest_text" class="form-control" name="quest_text" placeholder="Circumstances are out of your control, how you react is."></textarea>
 			</div>
 			<div class="form-group">
+				<label for="quest_name">Your Name or Handle</label>
+				<input id="quest_name" placeholder="Name" name="quest_name" class="form-control" />
+			</div>
+			<div class="form-group">
 				<input class="btn btn-primary btn-block" type="submit" value="Do Your Duty" />
 			</div>
 		</form>
+	</div>
+</div>
+
+<div class="row content" style="margin-top: 40px;">
+	<div class="col-sm-12">
+		<h2>What Others Have Shared</h2>
+		<?php
+			$args = array( 'post_type' => 'quest', 'posts_per_page' => -1 );
+			$loop = new WP_Query( $args );
+			if( $loop->have_posts() ) : while( $loop->have_posts() ) : $loop->the_post();
+		?>
+				<blockquote>
+					<?php the_content(); ?>
+					<em><?php the_title(); ?></em>
+				</blockquote>
+		<?php endwhile; endif; ?>
 	</div>
 </div>
 
--- includes/theme-enqueue.php
+++ includes/theme-enqueue.php
@@ -7,6 +7,15 @@ class my_theme_enqueue {
 		wp_enqueue_script( 'my-theme-main-js', MY_THEME_BUILD_URI. '/js/scripts.js', array( 'jquery' ), MY_THEME_VERSION, false );
 		wp_enqueue_style( 'my-theme-main-css', MY_THEME_BUILD_URI. '/css/styles.css', array(), MY_THEME_VERSION, 'all' );
 
+		$local_object = array(
+			'api_url'            => rest_get_url_prefix() . '/wp/v2/',
+			'template_directory' => get_template_directory_uri() . '/',
+			'nonce'              => wp_create_nonce( 'wp_rest' ),
+			'is_admin'           => current_user_can( 'administrator' ),
+			'site_title'         => get_bloginfo( 'name' ),
+		);
+
+		wp_localize_script( 'my-theme-main-js', 'wp_config', $local_object );
 	}
 
 }
