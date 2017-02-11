68275378bd9a325c0954ccd598ca836e862bc860
Roy Sivan <roy.sivan@disney.com>
2017-02-10 16:58:18 -0800

    search page

--- index.php
+++ index.php
@@ -1,10 +1,10 @@
-<?php get_header(); the_post(); ?>
-
+<?php get_header();  ?>
+<?Php if( have_posts() ) : while( have_posts() ) : the_post(); ?>
 <div class="row">
 	<div class="col-sm-12">
 		<h1><?php the_title(); ?></h1>
 		<?php the_content(); ?>
 	</div>
 </div>
-
+<?php endwhile; else: echo '<p>nothing found</p>'; endif; ?>
 <?php get_footer(); ?>
---
+++ page-search.php
@@ -0,0 +1,34 @@
+<?php get_header(); the_post(); ?>
+
+<div class="row">
+	<div class="col-sm-12">
+		<form class="form" id="share_quest_form" method="GET" action="/">
+			<input type="hidden" name="post_type" value="quest" />
+			<div class="form-group">
+				<label for="quest_name">Search For</label>
+				<input required id="quest_name" placeholder="Search For..." name="s" class="form-control" />
+			</div>
+			<div class="form-group">
+				<input class="btn btn-primary btn-block" type="submit" value="Schearch Well" />
+			</div>
+		</form>
+	</div>
+</div>
+
+<div class="row content" style="margin-top: 40px;">
+	<div class="col-sm-12">
+		<h2>What Others Have Shared</h2>
+		<?php
+			$args = array( 'post_type' => 'quest', 'posts_per_page' => -1 );
+			$loop = new WP_Query( $args );
+			if( $loop->have_posts() ) : while( $loop->have_posts() ) : $loop->the_post();
+		?>
+				<blockquote>
+					<?php the_content(); ?>
+					<em><?php the_title(); ?></em>
+				</blockquote>
+		<?php endwhile; endif; ?>
+	</div>
+</div>
+
+<?php get_footer(); ?>
